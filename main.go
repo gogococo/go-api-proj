6c06e38be5b6ac445aa487edca41cae7d8ff6e4e
Jacquie <Jacquelyne.grindrod@gmail.com>
2019-12-07 16:50:53 -0500

    Created simple handler func that serves Hello World at :8080

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	mux := mux.NewRouter()
+	mux.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(rw, "Hello World!")
+	})
+	// func listen_and_serve, pass in address ex port 8080, and router (mux)
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
 
-//mux := mux.NewRouter()
 // github.com/gorilla/mux
